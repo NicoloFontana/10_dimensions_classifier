ff363e5e4c0eb64102756cad4407a8143104329f
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2023-11-07 16:33:09 +0100

    Modify .gitignore. Add custom notebooks for classification. Add sentence extractor script.

--- sentences_extractor.py
+++ sentences_extractor.py
@@ -1,19 +1,27 @@
 import csv
 import json
 import random
+import sys
+from datetime import datetime as dt
 
 ## from Reddit conversations
-file = open('C:\\Users\\fonta\\PycharmProjects\\10_dimensions_classifier\\data\\cornell_reddit_corpus_small\\utterances.json')
+file = open(
+    'C:\\Users\\fonta\\PycharmProjects\\10_dimensions_classifier\\data\\cornell_reddit_corpus_small\\utterances.json')
 corpus = json.load(file)
-enough_words = [s for s in corpus if ((s['text'].count(' ') + s['text'].count('\t') + s['text'].count('\n')) > 9 and
-                                      (s['text'].count(' ')+s['text'].count('\t')+s['text'].count('\n')) < 12)]
-print(f"#long_enough: {len(enough_words)}")
-samples = random.sample(enough_words, k=10)
-print(samples)
+
+words_LB = 10
+words_UB = 15
+enough_words = [s for s in corpus if
+                ((s['text'].count(' ') + s['text'].count('\t') + s['text'].count('\n')) > words_LB and
+                 (s['text'].count(' ') + s['text'].count('\t') + s['text'].count('\n')) < words_UB)]
+# print(f"#long_enough: {len(enough_words)}")
+k = 10
+samples = random.sample(enough_words, k=k)
+# print(samples)
 samples_min = [{"id": sample["id"], "text": sample["text"]} for sample in samples]
-print(samples_min)
+# print(samples_min)
 samples_min_dict = {sample["id"]: sample["text"] for sample in samples_min}
-print(samples_min_dict)
+# print(samples_min_dict)
 
 # for s in samples:
 #     ## printing debug
@@ -26,8 +34,18 @@ print(samples_min_dict)
 #     print(f"#words: {words}")
 #     print(f"#chars: {len(s['text'])}")
 
-with open('C:\\Users\\fonta\\PycharmProjects\\10_dimensions_classifier\\out\\reddit_samples.txt', 'w') as convert_file:
-     convert_file.write(json.dumps(samples_min_dict))
+timestamp = dt.now().strftime("%Y%m%d%H%M")
+OUT_FILE_NAME = f"{k}_reddit_samples_{timestamp}.txt"
+OUT_FILE_PATH = f'C:\\Users\\fonta\\PycharmProjects\\10_dimensions_classifier\\out\\{OUT_FILE_NAME}'
+# print(OUT_FILE_PATH)
+
+with open(OUT_FILE_PATH, 'w') as convert_file:
+    convert_file.write(json.dumps(samples_min_dict))
+with open(OUT_FILE_PATH, 'a') as f:
+    f.write(
+        '\n\nAnswer with a single JSON-formatted text containing the id, the text and the scores for each sentence.')
+
+print(f"Generated file {OUT_FILE_NAME} of {k} sentences between {words_LB} and {words_UB} words")
 
 ## from Twitter sentiment analysis
 # with open('C:\\Users\\fonta\\PycharmProjects\\10_dimensions_classifier\\data\\sentiment140\\testdata.manual.2009.06.14.csv', newline='') as csvfile:
